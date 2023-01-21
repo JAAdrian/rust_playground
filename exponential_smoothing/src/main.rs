61c0591ed88b217188d75e3a52d3a6fe99494f72
Jens-Alrik Adrian <jensalrik.adrian@gmail.com>
2023-01-21 10:26:55 +0100

    Read signal file <phew>

--- exponential_smoothing/src/main.rs
+++ exponential_smoothing/src/main.rs
@@ -1,3 +1,23 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+const FILE_PATH: &str = "data/noisy_input_fs1000Hz.txt";
+
+/// Read a TXT file's context into a float64 vector.
+fn read_signal(file_path: &str) -> Vec<f64> {
+    let file = File::open(file_path).expect("File was not found.");
+    let reader = BufReader::new(file);
+
+    let content: Vec<f64> = reader
+        .lines()
+        .map(|line| line.unwrap().parse::<f64>().unwrap())
+        .collect();
+
+    return content;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let signal = read_signal(FILE_PATH);
+
+    println!("{:?}", &signal[0..10]);
 }
