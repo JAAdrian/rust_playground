3cb6533c18a4622e4ad0cd517510db33936067e4
Jens-Alrik Adrian <jensalrik.adrian@gmail.com>
2023-01-27 23:39:10 +0100

    Working smoother

--- exponential_smoothing/src/main.rs
+++ exponential_smoothing/src/main.rs
@@ -4,6 +4,10 @@ use std::io::{BufRead, BufReader};
 mod smoothing;
 
 const FILE_PATH: &str = "data/noisy_input_fs1000Hz.txt";
+const SAMPLE_RATE: i32 = 1000;
+
+const TIME_CONSTANT: f64 = 1.0;
+
 
 /// Read a TXT file's context into a float64 vector.
 fn read_signal(file_path: &str) -> Vec<f64> {
@@ -22,5 +26,18 @@ fn main() {
     let signal = read_signal(FILE_PATH);
     println!("{:?}", &signal[0..10]);
 
-    let mut smoother = smoothing::ExponentialSmoother{parent:smoothing::Smoother};
+    let mut smoother = smoothing::ExponentialSmoother::new();
+
+    smoother.set_initial_state(signal[0]);
+    smoother.set_time_contant(TIME_CONSTANT);
+    smoother.set_sample_rate(SAMPLE_RATE);
+    smoother.setup();
+
+    let mut smoothed_signal: Vec<f64> = Vec::with_capacity(signal.len());
+    for sample in signal {
+        smoothed_signal.push(smoother.step(sample));
+    }
+
+    println!("\n");
+    println!("{:?}", &smoothed_signal[0..10]);
 }
--- exponential_smoothing/src/smoothing.rs
+++ exponential_smoothing/src/smoothing.rs
@@ -5,22 +5,31 @@ fn compute_alpha(time_constant: f64, sample_rate: i32) -> f64 {
 }
 
 
-struct Smoother {
+pub struct Smoother {
     sample_rate: i32,
     initial_state: f64,
     time_constant: f64,
-    previous_value: f64,
+    previous_value: f64
 }
 
 impl Smoother {
-    fn new() -> Self {
-        Self{
+    pub fn new() -> Smoother {
+        Smoother{
             sample_rate: 0,
             initial_state: 0.0,
             time_constant: 0.0,
             previous_value: 0.0
         }
     }
+
+    pub fn setup(&mut self) {
+        self.previous_value = self.initial_state;
+    }
+
+    pub fn set_sample_rate(&mut self, rate: i32) {
+        self.sample_rate = rate;
+    }
+
     pub fn set_initial_state(&mut self, state: f64) {
         self.initial_state = state;
     }
@@ -32,11 +41,19 @@ impl Smoother {
 
 pub struct ExponentialSmoother {
     parent: Smoother,
-    alpha: f64,
+    alpha: f64
 }
 
 impl ExponentialSmoother {
+    pub fn new() -> ExponentialSmoother {
+        ExponentialSmoother {
+            parent: Smoother::new(),
+            alpha: 0.0
+        }
+    }
+
     pub fn setup (&mut self) {
+        self.parent.setup();
         self.alpha = compute_alpha(self.parent.time_constant, self.parent.sample_rate);
     }
 
@@ -49,5 +66,15 @@ impl ExponentialSmoother {
         return smoothed_sample;
     }
 
+    pub fn set_sample_rate(&mut self, rate: i32) {
+        self.parent.set_sample_rate(rate);
+    }
 
+    pub fn set_initial_state(&mut self, state: f64) {
+        self.parent.set_initial_state(state);
+    }
+
+    pub fn set_time_contant(&mut self, constant: f64) {
+        self.parent.set_time_contant(constant);
+    }
 }
